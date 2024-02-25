fe4f2937e1f3a10df6b5c262d4ba636530f4be46
Dmitry Budyakov <dnikbu@yandex.ru>
2024-02-26 02:24:51 +0300

    Seminar 11, Homework, Base task

---
+++ src/main/java/com/example/spring_sem11_hw/SpringSem11HwApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_sem11_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSem11HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSem11HwApplication.class, args);
+	}
+
+}
